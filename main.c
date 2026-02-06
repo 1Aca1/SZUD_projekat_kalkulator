f11aa6313a0a3f59e440f3d3a3c43fdfd184b728
1Aca1 <57568159+1Aca1@users.noreply.github.com>
2026-02-06 02:47:52 +0100

    Dodavanje funkcije za sabiranje

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
 
+int saberi(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    int izbor;
+    int izbor, a, b, rezultat;
 
     printf("Kalkulator\n");
     printf("1 - Sabiranje\n");
@@ -10,5 +14,14 @@ int main() {
     printf("Izaberite opciju: ");
     scanf("%d", &izbor);
 
+    if (izbor == 1) {
+        printf("Unesite dva broja: ");
+        scanf("%d %d", &a, &b);
+        rezultat = saberi(a, b);
+        printf("Rezultat: %d\n", rezultat);
+    }
+
     return 0;
 }
+
+
