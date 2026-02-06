6549005b46b9a3177462615c3d22ce98431580ed
1Aca1 <57568159+1Aca1@users.noreply.github.com>
2026-02-06 02:46:42 +0100

    Dodavanje osnovnog menija

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 
 int main() {
+    int izbor;
+
     printf("Kalkulator\n");
+    printf("1 - Sabiranje\n");
+    printf("2 - Oduzimanje\n");
+    printf("Izaberite opciju: ");
+    scanf("%d", &izbor);
+
     return 0;
 }
